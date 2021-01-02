b0a3de3860337cdbba58321f74eccc8c21a14015
lukelee <lukelee01@gmail.com>
2021-01-02 16:10:22 -0500

    initial commit

---
+++ dogmicroservice/src/test/java/com/example/dogmicroservice/DogmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dogmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogmicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ dogs/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO dog (name, breed, origin) VALUES ('dog01', 'breed01', 'origin01');
+INSERT INTO dog (name, breed, origin) VALUES ('dog02', 'breed02', 'origin02');
+INSERT INTO dog (name, breed, origin) VALUES ('dog03', 'breed03', 'origin03');
+INSERT INTO dog (name, breed, origin) VALUES ('dog03', 'breed03', 'origin03');
+INSERT INTO dog (name, breed, origin) VALUES ('dog04', 'breed04', 'origin04');
+INSERT INTO dog (name, breed, origin) VALUES ('dog05', 'breed05', 'origin05');
+INSERT INTO dog (name, breed, origin) VALUES ('dog06', 'breed06', 'origin06');
+INSERT INTO dog (name, breed, origin) VALUES ('dog07', 'breed07', 'origin07');
+INSERT INTO dog (name, breed, origin) VALUES ('dog08', 'breed08', 'origin08');
+INSERT INTO dog (name, breed, origin) VALUES ('dog09', 'breed09', 'origin09');
+INSERT INTO dog (name, breed, origin) VALUES ('dog10', 'breed10', 'origin10');
+
